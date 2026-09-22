e6e9623c04a466e7cef36dc50e10ea9d9ff1a880
pedrorebollo <rebopsj@gmail.com>
2026-09-22 19:53:34 -0300

    Update presentacion.py

--- tp2/presentacion.py
+++ tp2/presentacion.py
@@ -4,11 +4,6 @@ Módulo encargado de la presentación tabular de los registros.
 
 
 def abreviar_texto(texto, longitud_maxima):
-    """
-    Recorta un texto a la longitud máxima indicada, agregando puntos
-    suspensivos cuando corresponde, para conservar la alineación de
-    las columnas en la tabla.
-    """
     if len(texto) > longitud_maxima:
         resultado = texto[:longitud_maxima - 3] + "..."
     else:
@@ -17,7 +12,6 @@ def abreviar_texto(texto, longitud_maxima):
 
 
 def mostrar_tabla(matriz):
-    """Muestra los registros de la matriz en formato de tabla, alineada y con encabezados."""
     encabezado = f"{'Código':<8}{'Título':<26}{'Género':<17}{'Puntuación':<12}{'Reseña':<50}"
     print(encabezado)
     print("-" * len(encabezado))
