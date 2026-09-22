d8b914549dcf1c9b86c2dda3a05b4ac1b0ec57d8
pedrorebollo <rebopsj@gmail.com>
2026-09-22 19:56:43 -0300

    Update cadenas.py

--- tp2/cadenas.py
+++ tp2/cadenas.py
@@ -5,13 +5,8 @@ Módulo encargado del procesamiento de cadenas sobre el texto principal
 
 
 def separar_en_palabras(texto):
-    """Separa un texto en una lista de palabras, usando el espacio como separador."""
     return texto.split()
 
 
 def reconstruir_texto(lista_palabras, separador):
-    """
-    Reconstruye una cadena a partir de una lista de palabras, uniéndolas
-    con el separador indicado.
-    """
     return separador.join(lista_palabras)
