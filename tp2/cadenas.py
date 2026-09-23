895892944b1fb34be649004d466e0dfd86004fa3
lucasdoria88 <lukaslautidoria@gmail.com>
2026-09-23 20:59:19 -0300

    Update cadenas.py

--- tp2/cadenas.py
+++ tp2/cadenas.py
@@ -18,3 +18,17 @@ def capitalizar_texto(texto):
 
 def normalizar_texto_a_minusculas(texto):
     return texto.lower()
+
+
+def obtener_vista_previa(texto, cantidad_caracteres):
+    inicio = texto[:cantidad_caracteres]
+    final = texto[-cantidad_caracteres:]
+    return inicio, final
+
+
+def eliminar_espacios_extremos(texto):
+    return texto.strip()
+
+
+def reemplazar_palabra(texto, palabra_original, palabra_nueva):
+    return texto.replace(palabra_original, palabra_nueva)
