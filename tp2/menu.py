322fb793c7f5b4574c1662d634fd2ebceb49924e
lucasdoria88 <lukaslautidoria@gmail.com>
2026-09-23 21:01:11 -0300

    Update menu.py

--- tp2/menu.py
+++ tp2/menu.py
@@ -4,9 +4,13 @@ Módulo encargado de mostrar el menú de opciones disponibles.
 
 
 def mostrar_menu():
-    """Muestra por pantalla las opciones del menú principal disponibles en esta entrega."""
     print("\n===== SISTEMA DE RESEÑAS DE PELÍCULAS Y SERIES =====")
-    print("1. Separar y reconstruir un texto")
-    print("2. Consultar registros por categoría")
-    print("3. Procesamiento estadístico")
+    print("1. Mostrar todos los registros")
+    print("2. Consultar un registro por código")
+    print("3. Generar una vista previa del texto")
+    print("4. Normalizar y transformar un texto")
+    print("5. Consultar registros por palabra o expresión")
+    print("6. Separar y reconstruir un texto")
+    print("7. Consultar registros por categoría")
+    print("8. Procesamiento estadístico")
     print("0. Salir")
